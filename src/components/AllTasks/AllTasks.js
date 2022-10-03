763763c92f571b761697f44eae0f8c16f2a9a073
Jisan Hasan <jisan.jh5@gmail.com>
2022-10-03 13:15:49 +0600

    display all the tasks

--- src/components/AllTasks/AllTasks.js
+++ src/components/AllTasks/AllTasks.js
@@ -13,7 +13,7 @@ const AllTasks = () => {
         <div className='py-14 px-20'>
             <div className='text-3xl text-bold text-primary mb-5'><h1>StudyPlanner</h1></div>
             <p>Select your Study Topic Now.</p>
-            <div>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-3 w-full h-full'>
                 {
                     tasks.map(task => <SingleTask task={task} key={task.id}></SingleTask>)
                 }
--- src/components/SingleTask/SingleTask.js
+++ src/components/SingleTask/SingleTask.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './SingleTask.css';
+import React from "react";
+import "./SingleTask.css";
 
-const SingleTask = ({task}) => {
-    return (
-        <div>
-            <h1>{task.name}</h1>
+const SingleTask = ({ task }) => {
+  const { img, name, time } = task;
+  return (
+    <div>
+      <div className="card w-full h-full bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10 w-full h-full">
+          <img src={img} alt={name} className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{name}</h2>
+          <p>Time: {time} Min.</p>
+          <div className="card-actions">
+            <button className="btn btn-primary px-10">Add To List</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SingleTask;
