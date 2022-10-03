7521b71cd312a7dd44d2d679963002782b4fce86
Jisan Hasan <jisan.jh5@gmail.com>
2022-10-03 23:04:15 +0600

    font-awesome icon added

--- src/components/AllTasks/AllTasks.js
+++ src/components/AllTasks/AllTasks.js
@@ -1,25 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import SingleTask from '../SingleTask/SingleTask';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect, useState } from "react";
+import SingleTask from "../SingleTask/SingleTask";
 import "./AllTasks.css";
 
-const AllTasks = ({handleAddToList}) => {
-    const [tasks, setTasks] = useState([]);
-    useEffect(() => {
-        fetch(`fakedata.json`)
-        .then(res => res.json())
-        .then(data => setTasks(data));
-    }, [])
-    return (
-        <div className='py-14 px-20'>
-            <div className='text-3xl text-bold text-primary mb-5'><h1>StudyPlanner</h1></div>
-            <p>Select your Study Topic Now.</p>
-            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-3 w-full h-full'>
-                {
-                    tasks.map(task => <SingleTask task={task} key={task.id} handleAddToList={handleAddToList}></SingleTask>)
-                }
-            </div>
-        </div>
-    );
+const AllTasks = ({ handleAddToList }) => {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    fetch(`fakedata.json`)
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  }, []);
+  return (
+    <div className="py-14 px-20">
+      <div className="flex gap-2 text-3xl text-bold text-primary mb-5">
+        <FontAwesomeIcon icon={faBook} />
+        <h1>StudyPlanner</h1>
+      </div>
+      <p className="mb-3">Select your Study Topic Now.</p>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 w-full h-full">
+        {tasks.map((task) => (
+          <SingleTask
+            task={task}
+            key={task.id}
+            handleAddToList={handleAddToList}
+          ></SingleTask>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllTasks;
--- src/components/SingleTask/SingleTask.js
+++ src/components/SingleTask/SingleTask.js
@@ -5,7 +5,7 @@ const SingleTask = ({ task, handleAddToList }) => {
   const { img, name, time } = task;
   return (
     <div>
-      <div className="card w-full h-full bg-base-100 shadow-xl">
+      <div className="card w-full h-full bg-amber-100 shadow-xl">
         <figure className="px-10 pt-10 w-full h-full">
           <img src={img} alt={name} className="rounded-xl" />
         </figure>
