6550fd93455e5e3645f8b54e407c481b6f2641f3
Jisan Hasan <jisan.jh5@gmail.com>
2022-10-03 17:40:38 +0600

    save data on localstorage

--- src/components/StatusPanel/StatusPanel.js
+++ src/components/StatusPanel/StatusPanel.js
@@ -2,26 +2,35 @@ import React, { useState } from "react";
 import "./StatusPanel.css";
 
 const StatusPanel = ({studyTime}) => {
-  const [breakTime, setBreakTime] = useState(0);
+
+  let lsBreakTime = Number(localStorage.getItem("breakTime"));
+  if(!lsBreakTime){
+    lsBreakTime = 0;
+  }
+  const [breakTime, setBreakTime] = useState(lsBreakTime);
 
   
+  const handleBreakTime = time => {
+    localStorage.setItem("breakTime", time);
+    setBreakTime(time);
+  }
   return (
     <div className="bg-slate-400 px-6 py-10">
       <h3 className="text-2xl my-4">Add a Break</h3>
       <div className="bg-orange-100 rounded p-5 flex justify-between">
-        <button onClick={() => setBreakTime(10)} className="btn btn-primary btn-sm rounded-full">
+        <button onClick={() => handleBreakTime(10)} className="btn btn-primary btn-sm rounded-full">
           10M
         </button>
-        <button onClick={() => setBreakTime(15)} className="btn btn-primary btn-sm rounded-full">
+        <button onClick={() => handleBreakTime(15)} className="btn btn-primary btn-sm rounded-full">
           15M
         </button>
-        <button onClick={() => setBreakTime(20)} className="btn btn-primary btn-sm rounded-full">
+        <button onClick={() => handleBreakTime(20)} className="btn btn-primary btn-sm rounded-full">
           20M
         </button>
-        <button onClick={() => setBreakTime(25)} className="btn btn-primary btn-sm rounded-full">
+        <button onClick={() => handleBreakTime(25)} className="btn btn-primary btn-sm rounded-full">
           25M
         </button>
-        <button onClick={() => setBreakTime(30)} className="btn btn-primary btn-sm rounded-full">
+        <button onClick={() => handleBreakTime(30)} className="btn btn-primary btn-sm rounded-full">
           30M
         </button>
       </div>
